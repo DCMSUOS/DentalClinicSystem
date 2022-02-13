6b59214607b64676bb0c2b0519f721f5d741f55d
hariiad <hariadrahim@gmail.com>
2022-02-13 23:39:16 +0300

    add more color

--- src/assets/color/Colors.js
+++ src/assets/color/Colors.js
@@ -2,4 +2,8 @@ export default {
   blueColor: "#2e78e9", //rgba(46, 120, 233,1)
   lightBackgroundColor: "#f1f5f8",
   darkBackgroudColor: "#212b37", //rgba(33, 43, 55,0.1)
+  redColor: "#e94c3c", //RGB(233, 76, 60)
+  greenColor: "#1eb65e", //RGB(30, 182, 94)
+  orangeColor: "#e07924", // RGB(224, 121, 36)
+  purple: "#a973cd", //RGB(169, 115, 205)
 };
